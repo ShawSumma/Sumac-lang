0464c084babd6dd8e5eecf892a6d9017e9fa5726
Henrik Böving <hargonix@gmail.com>
2018-04-02 21:40:02 +0200

    flake8

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import re
 
+
 class Token:
     def __init__(self, type, match):
         global code
         global col
-        #print(match)
-        if isinstance(match,str):
+        # print(match)
+        if isinstance(match, str):
             end_pos = len(match)
         else:
             end_pos = match.span()[1]
         self.raw_data = code[:end_pos]
-        if self.raw_data in ['int','char','bool','if','while','for','loop','else','elif']:
+        keywords = ['int', 'char', 'bool', 'if', 'while', 'for', 'loop',
+                    'else', 'elif']
+        if self.raw_data in keywords:
             type = 'keyword'
         self.type = type
         self.line = line
         col += end_pos
         code = code[end_pos:]
+
     def __str__(self):
-        return 'token({}) : {}'.format(self.type,self.raw_data)
+        return 'token({}) : {}'.format(self.type, self.raw_data)
     __repr__ = __str__
+
 
 def view_tokens(tokens):
     for i in tokens:
         print(i)
+
 
 def macro(code):
     pl = 0
@@ -32,11 +38,11 @@ def macro(code):
             pl += 1
             mat = ''
             while pl < len(code) and code[pl] != '`':
-                #print(code[pl])
+                # print(code[pl])
                 code = code
                 mat += code[pl]
                 pl += 1
-            #print(mat)
+            # print(mat)
             if mat.startswith('include'):
                 fopen = open(mat[len('include')+1:])
                 new_code = fopen.read()
@@ -49,6 +55,8 @@ def macro(code):
                 pl += 1
         pl += 1
     return code
+
+
 def tokenize(file):
     global code
     global line
@@ -59,8 +67,8 @@ def tokenize(file):
         'name': r'[a-zA-Z_]+[a-zA-Z]*',
         'int': r'[0-9]+',
         'float': r'([0-9]+\.[0-9]*)|([0-9]*\.[0-9]+)',
-        #'operator': r'[+-*/!&<>^%]+ ', # make a list containing the types later
-
+        # 'operator': r'[+-*/!&<>^%]+ '
+        # Make a list containing the types later
     }
     compiled_regexes = {}
     for i in match_regexes:
@@ -70,7 +78,7 @@ def tokenize(file):
                  '<=', '>=', '+=', '-=', '*=', '/=', '=', '==', '!=']
     operators.sort(key=lambda op: 100-len(op))
 
-    brace_types = '{}()[]'
+    brace_types = '\{\}()[]'
     brace_names = {
         '(': 'l paren',
         ')': 'r paren',
@@ -79,13 +87,13 @@ def tokenize(file):
         '[': 'r list',
         ']': 'l list',
     }
-    #print(compiled_regexes)
+    # print(compiled_regexes)
     return_tokens = []
     line = 1
     col = 1
-    #loop until no tokens
+    # loop until no tokens
     while 1:
-        #print(code)
+        # print(code)
         end_flag = True
 
         while len(code) > 0 and code[0] in '\n\t ':
@@ -99,15 +107,15 @@ def tokenize(file):
             return return_tokens
 
         if code[0] in brace_types:
-            return_tokens.append(Token(brace_names[code[0]],code[0]))
+            return_tokens.append(Token(brace_names[code[0]], code[0]))
             end_flag = True
             continue
 
         if end_flag:
             for i in operators:
-                #print(i,[code[:len(i)],i],code[:len(i)] == i)
+                # print(i,[code[:len(i)],i],code[:len(i)] == i)
                 if code[:len(i)] == i:
-                    return_tokens.append(Token('operator',i))
+                    return_tokens.append(Token('operator', i))
                     end_flag = False
                     continue
 
@@ -117,15 +125,17 @@ def tokenize(file):
 
         if end_flag:
             for type in matches:
-                if matches[type] != None:
-                    #print('found a match')
-                    return_tokens.append(Token(type,matches[type]))
+                if matches[type] is not None:
+                    # print('found a match')
+                    return_tokens.append(Token(type, matches[type]))
                     end_flag = False
                     continue
 
         if end_flag:
             break
-    print("lex er on line %s at col %s" % (line,col))
+    print("lex er on line %s at col %s" % (line, col))
     exit()
+
+
 tokens = tokenize('source.txt')
 view_tokens(tokens)
