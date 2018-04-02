5c1fad4f214865e87b63fa4df282e86a73955a8e
Xeno <shawsumma@gmail.com>
2018-04-02 12:05:58 -0400

    fix the darn bug

--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ int main(){
       else{
         tokenStrings.insert(tokenStrings.begin(),currentToken);
         int tokenTypeStr;
-        if (currentToken != "int"){
+        if (currentToken != "int"){/// || currentToken != "str"){
           tokenTypeStr = 3;
         }
         else {
